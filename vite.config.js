35e0f10c3346a5a7146a00c3c5293cf7996b12ca
Franklin Boachie-Yiadom <franklinboachie@gmail.com>
2025-12-29 16:07:36 +0500

    added base path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [tailwindcss()],
   build: {
     outDir: "dist",
-    base: "/",
+    base: "/snb_cleaning/",
     rollupOptions: {
       input: {
         main: "index.html",
